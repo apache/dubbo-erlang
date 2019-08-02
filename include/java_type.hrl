3400d13662791df1adea51aa6af8cf00e7793302
DLive <xsxgmail@163.com>
2019-08-02 16:38:00 +0800

    add java exception define

--- include/java_type.hrl
+++ include/java_type.hrl
@@ -14,6 +14,38 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 %%------------------------------------------------------------------------------
--record(null_pointer_exception,{detailMessage,cause,stackTrace,suppressedExceptions}).
-
--record(stack_stack_trace_element,{declaringClass,methodName,fileName,lineNumber}).
+-record(java_stack_trace_element, {declaringClass, methodName, fileName, lineNumber}).
+-record(java_null_pointer_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_runtime_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(index_out_bounds_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_string_index_out_bounds_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_array_index_out_bounds_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_arithmetic_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_class_cast_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_nio_provider_not_found_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_security_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_annotation_type_mismatch_exception, {detailMessage, cause, stackTrace, suppressedExceptions, element, foundType}).
+-record(dubbo_rpc_exception, {detailMessage, cause, stackTrace, suppressedExceptions, code}).
+-record(java_enum_constant_not_present_exception, {detailMessage, cause, stackTrace, suppressedExceptions, enumType, constantName}).
+-record(java_no_such_element_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_input_mismatch_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(dubbo_hessian_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_wrong_method_type_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_incomplete_annotation_exception, {detailMessage, cause, stackTrace, suppressedExceptions, annotationType, elementName}).
+-record(java_malformed_parameters_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_undeclared_throwable_exception, {detailMessage, cause, stackTrace, suppressedExceptions, undeclaredThrowable}).
+-record(dubbo_no_such_property_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_mirrored_types_exception, {detailMessage, cause, stackTrace, suppressedExceptions, types}).
+-record(dubbo_no_such_method_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_unchecked_io_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_illegal_monitor_state_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_negative_array_size_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_unsupported_operation_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_empty_stack_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_illegal_state_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_datetime_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_completion_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_malformed_parameterized_type_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(dubbo_service_generic_exception, {detailMessage, cause, stackTrace, suppressedExceptions, exceptionClass, exceptionMessage}).
+-record(java_illegal_argument_exception, {detailMessage, cause, stackTrace, suppressedExceptions}).
+-record(java_missing_resource_pointer_exception, {detailMessage, cause, stackTrace, suppressedExceptions, className, key}).
